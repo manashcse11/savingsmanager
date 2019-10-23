372da8828d3b61b1f57a21a5826d47bf96f8d81d
Manash <manash@cefalo.com>
2019-10-23 18:26:42 +0600

    Add data table for transaction

--- resources/views/transaction/list.blade.php
+++ resources/views/transaction/list.blade.php
@@ -14,7 +14,92 @@
                         </div>
                     @endif
 
-                    Here is your list
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th style="vertical-align: top !important; " scope="col">#</th>
+                                <th style="vertical-align: top !important; " scope="col">
+                                    Type
+                                    <select class="form-control form-control-sm">
+                                        <option>All</option>
+                                        <option>DPS</option>
+                                        <option>Sanchaypatra</option>
+                                    </select>
+                                </th>
+                                <th style="vertical-align: top !important; " scope="col">
+                                    Organization
+                                    <select class="form-control form-control-sm">
+                                        <option>All</option>
+                                        <option>DBBL</option>
+                                        <option>Post Office</option>
+                                        <option>BD Bank</option>
+                                        <option>EBL</option>
+                                    </select>
+                                </th>
+                                <th style="vertical-align: top !important; " scope="col">Amount</th>
+                                <th style="vertical-align: top !important; " scope="col">Start Date</th>
+                                <th style="vertical-align: top !important; " scope="col">Duration</th>
+                                <th style="vertical-align: top !important; " scope="col">
+                                    Status
+                                    <select class="form-control form-control-sm">
+                                        <option>All</option>
+                                        <option>Running</option>
+                                        <option>Matured</option>
+                                    </select>
+                                </th>
+                                <th style="vertical-align: top !important; " scope="col">
+                                    Mature Date
+                                    <div class="form-group row">
+                                        <div class="col-sm-10">
+                                            <button type="button" class="btn btn-primary btn-sm">Search</button>
+                                        </div>
+                                    </div>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>DPS</td>
+                                <td>EBL</td>
+                                <td>10000</td>
+                                <td>03 September, 2019</td>
+                                <td>5</td>
+                                <td>Running</td>
+                                <td>03 September, 2024</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>DPS</td>
+                                <td>EBL</td>
+                                <td>5000</td>
+                                <td>03 September, 2019</td>
+                                <td>5</td>
+                                <td>Running</td>
+                                <td>03 September, 2024</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Sanchaypatra</td>
+                                <td>BD Bank</td>
+                                <td>100000</td>
+                                <td>09 September, 2019</td>
+                                <td>5</td>
+                                <td>Running</td>
+                                <td>09 September, 2024</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>San</td>
+                                <td>DBBL</td>
+                                <td>10000</td>
+                                <td>03 September, 2019</td>
+                                <td>5</td>
+                                <td>Running</td>
+                                <td>03 September, 2024</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
