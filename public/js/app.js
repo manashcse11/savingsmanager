4277dd1d9eaed2f26f9f2b3bc954781383041b3d
Manash Chakrobortty <manash.pstu@gmail.com>
2019-11-08 23:03:20 +0600

    Report: Increase border for each year row

--- public/js/app.js
+++ public/js/app.js
@@ -51969,8 +51969,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/savingsmanager/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/savingsmanager/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\savings\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\savings\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
