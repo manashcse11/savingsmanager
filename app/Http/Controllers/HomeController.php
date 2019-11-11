9068cebdeaa290ed056b0558157585ef2941afda
Manash <manash@cefalo.com>
2019-11-11 18:25:11 +0600

    Add google chart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,31 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data['options'] = [
+            'title' => 'Population of Largest U.S. Cities',
+            'chartArea' => ['width' => '50%'],
+            'hAxis' => [
+                'title' => 'Total Population',
+                'minValue' => 0
+            ],
+            'vAxis' => [
+                'title' => 'City'
+            ],
+            'bars' => 'horizontal', //required if using material chart
+            'axes' => [
+                'y' => [0 => ['side' => 'right']]
+            ]
+        ];
+
+        $data['cols'] = ['City', '2010 Population', '2000 PopulaÎtions'];
+        $data['rows'] = [
+            ['New York City, NY', 8175000, 8008000],
+            ['Los Angeles, CA', 3792000, 3694000],
+            ['Chicago, IL', 2695000, 2896000],
+            ['Houston, TX', 2099000, 1953000],
+            ['Philadelphia, PA', 1526000, 1517000]
+        ];
+
+        return view('home', $data);
     }
 }
